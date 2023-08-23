1ea88217a7e1c7a00c063593f922217c324c9a02
Codersoumya26 <Panjasoumya@gmail.com>
2023-08-23 23:21:50 +0530

    Added besic css and bg images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from "./components/header/Header"
+import Navbar from "./components/navbar/Navbar"
+import About from "./components/about/About"
+import Experience from "./components/experience/Experience"
+import Services from "./components/services/Services"
+import Portfolio from "./components/portfolio/Portfolio"
+import Testimonials from "./components/testimonials/Testimonials"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/footer/Footer"
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header />
+        <Navbar />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+    </>
   )
 }
 
