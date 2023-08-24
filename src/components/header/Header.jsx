18efd0f193a8f9ae7a69d6bc0c9d913b62a829dd
Codersoumya26 <Panjasoumya@gmail.com>
2023-08-24 11:13:34 +0530

    Added HeaderSocial and ReactIcons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import HeaderButton from './HeaderButton'
 import me2 from '../../assets/me2.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -12,7 +13,8 @@ const Header = () => {
         <h5 className="text-light"> Fullstack Developer </h5>
         
         <HeaderButton />
-      
+        <HeaderSocials />
+
       <div className="me">
         <img src={me2} alt="Profile" />
       </div>
