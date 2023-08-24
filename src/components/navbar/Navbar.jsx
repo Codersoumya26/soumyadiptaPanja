473e414318b2f74027cd377b77dd75a96cf35aeb
Codersoumya26 <Panjasoumya@gmail.com>
2023-08-24 16:11:13 +0530

    Add navbar css and Update navbar with useState

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react'
 import "./navbar.css"
 import {FaHome, FaUserCircle, FaBookOpen} from "react-icons/fa"
 import {BiSolidMessage} from "react-icons/bi"
@@ -6,14 +7,51 @@ import {MdWorkHistory} from "react-icons/md"
 import {RiServiceLine} from "react-icons/ri"
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"> <FaHome /> </a>
-      <a href="#about"> <FaUserCircle /> </a>
-      <a href="#academic"> <FaBookOpen /> </a>
-      <a href="#experience"> <MdWorkHistory /> </a>
-      <a href="#services"> <RiServiceLine /> </a>
-      <a href="#contact"> <BiSolidMessage /> </a>
+      <a
+        href="#"
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : '' }
+      >
+        <FaHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav('about')}
+        className={activeNav === 'about' ? 'active' : '' }
+      >
+        <FaUserCircle />
+      </a>
+      <a
+        href="#academic"
+        onClick={() => setActiveNav('academic')}
+        className={activeNav === 'academic' ? 'active' : '' }
+      >
+        <FaBookOpen />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav('experience')}
+        className={activeNav === 'experience' ? 'active' : '' }
+      >
+        <MdWorkHistory />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav('services')}
+        className={activeNav === 'services' ? 'active' : '' }
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav('contact')}
+        className={activeNav === 'contact' ? 'active' : '' }
+      > 
+        <BiSolidMessage />
+      </a>
     </nav>
   )
 }
