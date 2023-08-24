a3194fbddeb11713876ba899a46909064e0ae2e7
Codersoumya26 <Panjasoumya@gmail.com>
2023-08-24 14:56:07 +0530

    Add navbar componenet

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./navbar.css"
+import {FaHome, FaUserCircle, FaBookOpen} from "react-icons/fa"
+import {BiSolidMessage} from "react-icons/bi"
+import {MdWorkHistory} from "react-icons/md"
+import {RiServiceLine} from "react-icons/ri"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <a href="#"> <FaHome /> </a>
+      <a href="#about"> <FaUserCircle /> </a>
+      <a href="#academic"> <FaBookOpen /> </a>
+      <a href="#experience"> <MdWorkHistory /> </a>
+      <a href="#services"> <RiServiceLine /> </a>
+      <a href="#contact"> <BiSolidMessage /> </a>
+    </nav>
   )
 }
 
