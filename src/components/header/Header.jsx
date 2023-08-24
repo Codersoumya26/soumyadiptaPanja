1c0d613d98e7d8fa168eb573b30dedfb7fac7a6b
Codersoumya26 <Panjasoumya@gmail.com>
2023-08-24 10:04:52 +0530

    Header component inital update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import "./header.css"
+import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5> Hello, I'm </h5>
+        <h1> Soumyadipta Panja </h1>
+        <h5 className="text-light"> Fullstack Developer </h5>
+      </div>
+    </header>
   )
 }
 
