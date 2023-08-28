ebf212f75fa0e8ff517da3b12c3794c84c1e6d7c
Codersoumya26 <Panjasoumya@gmail.com>
2023-08-28 17:56:29 +0530

    Add react-emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./contact.css"
 import { MdOutlineMail } from "react-icons/md"
 import { FaFacebookMessenger } from "react-icons/fa"
 import { BsWhatsapp } from "react-icons/bs"
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_arl22ac', 'template_qh3at99', form.current, '-lj7zi4CjLg2e94wf')
+    
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
       <h2> Lets Talk </h2>
@@ -13,26 +24,26 @@ const Contact = () => {
       <div className="container contact_container">
         <div className="contact_options">
           <article className="contact_option">
-            <MdOutlineMail />
+            <MdOutlineMail className='contact_option-icon'/>
               <h4> Email </h4>
               <h5> panjasoumya2@gmail.com </h5>
               <a href="mailto:panjasoumya2@gmail.com" target="__blank"> Send a message </a>
           </article>
           <article className="contact_option">
-            <FaFacebookMessenger />
+            <FaFacebookMessenger className='contact_option-icon'/>
               <h4> Messenger </h4>
               <h5> Soumya Dipta </h5>
               <a href="https://m.me/" target="__blank"> Send a message </a>
           </article>
           <article className="contact_option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact_option-icon'/>
               <h4> Whatsapp </h4>
               <h5> +916291491940 </h5>
               <a href="https://wa.me/+916291491940" target="__blank"> Send a message </a>
           </article>
         </div>
         {/* End of Contact Options */}
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
            <input type="text" name='name' placeholder='Your Full Name' required />
            <input type="email" name='email' placeholder='Your Mail-Id' required />
            <textarea name="message" rows="7" placeholder='Your Message' required> </textarea>
